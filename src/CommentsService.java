77c8ab8c7844c6bd2a189320ed3dd65b595ec63f
Tarek Morsi <tarekmorsi96@gmail.com>
2019-03-03 19:25:51 +0200

    Update comments service

--- src/CommentsService.java
+++ src/CommentsService.java
@@ -1,4 +1,102 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeoutException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import com.mongodb.client.MongoDatabase;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import Commands.Command;
+import Commands.CreateComment;
+import Commands.RetrieveComment;
 
 public class CommentsService {
+	private static final String RPC_QUEUE_NAME = "comment-request";
+	public static  MongoDatabase database;
+	public static void main(String[] argv) {
 
+		// initialize thread pool of fixed size
+		final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
+
+		ConnectionFactory factory = new ConnectionFactory();
+		String host = System.getenv("RABBIT_MQ_SERVICE_HOST");
+		factory.setHost(host);
+		Connection connection = null;
+		try {
+			connection = factory.newConnection();
+			final Channel channel = connection.createChannel();
+
+			channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+
+			System.out.println(" [x] Awaiting RPC requests");
+
+			Consumer consumer = new DefaultConsumer(channel) {
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+						byte[] body) throws IOException {
+					AMQP.BasicProperties replyProps = new AMQP.BasicProperties.Builder()
+							.correlationId(properties.getCorrelationId()).build();
+					System.out.println("Responding to corrID: " + properties.getCorrelationId());
+
+					try {
+						String message = new String(body, "UTF-8");
+						JSONParser parser = new JSONParser();
+						JSONObject messageBody = (JSONObject) parser.parse(message);
+						String command = (String) messageBody.get("command");
+						Command cmd = null;
+						System.out.println(command);
+						switch (command) {
+						case "CreateComment":
+							cmd = new CreateComment();
+							break;
+						case "RetrieveComment":
+							cmd = new RetrieveComment();
+							break;
+						}
+
+						HashMap<String, Object> props = new HashMap<String, Object>();
+						props.put("channel", channel);
+						props.put("properties", properties);
+						props.put("replyProps", replyProps);
+						props.put("envelope", envelope);
+						props.put("body", message);
+					
+						cmd.init(props);
+						executor.submit(cmd);
+					} catch (RuntimeException e) {
+						System.out.println(" [.] " + e.toString());
+					} catch (ParseException e) {
+						e.printStackTrace();
+					} finally {
+						synchronized (this) {
+							this.notify();
+						}
+					}
+				}
+			};
+
+			channel.basicConsume(RPC_QUEUE_NAME, true, consumer);
+		} catch (IOException | TimeoutException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static String getCommand(String message) throws ParseException {
+		JSONParser parser = new JSONParser();
+		JSONObject messageJson = (JSONObject) parser.parse(message);
+		String result = messageJson.get("command").toString();
+		return result;
+	}
+	public static MongoDatabase getDb() {
+		return database;
+	}
 }
--- src/Model/Comment.java
+++ src/Model/Comment.java
@@ -1,5 +1,78 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import Commands.Command;
+
 public class Comment {
 
+	private static final String COLLECTION_NAME = "comments";
+	private static final MongoClientURI uri = new MongoClientURI(
+			"mongodb://admin:admin@cluster0-shard-00-00-nvkqp.gcp.mongodb.net:27017,cluster0-shard-00-01-nvkqp.gcp.mongodb.net:27017,cluster0-shard-00-02-nvkqp.gcp.mongodb.net:27017/El-Menus?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
+
+
+	public static HashMap<String, Object> create(HashMap<String, Object> attributes, String target_id) {
+
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("El-Menus");
+
+		// Retrieving a collection
+		MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+		Document newComment = new Document();
+
+		for (String key : attributes.keySet()) {
+			newComment.append(key, attributes.get(key));
+		}
+
+		newComment.append("target_id", new ObjectId(target_id));
+		
+		collection.insertOne(newComment);
+		mongoClient.close();
+		
+		return attributes;
+	}
+
+
+	public static ArrayList<HashMap<String, Object>> get(String commentId, String type) {
+
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("El-Menus");
+
+		// Retrieving a collection
+		MongoCollection<Document> collection = database.getCollection("comments");
+		BasicDBObject query = new BasicDBObject();
+		query.put("target_id", new ObjectId(commentId));
+
+		FindIterable<Document> docs = collection.find(query);
+		JSONParser parser = new JSONParser(); 
+		ArrayList<HashMap<String, Object>> comments = new ArrayList<HashMap<String, Object>>();
+
+		for (Document document : docs) {
+			JSONObject json;
+			try {
+				json = (JSONObject) parser.parse(document.toJson());
+				HashMap<String, Object> comment = Command.jsonToMap(json);	
+				comments.add(comment);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		mongoClient.close();
+        return comments;
+		
+	}
 }

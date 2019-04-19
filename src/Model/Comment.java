56f60423334995816b0e9aec0a0882da2e09da46
hazemsalah <hazemsalah31296@hotmail.com>
2019-04-19 21:17:08 +0200

    Update Comment.java

--- src/Model/Comment.java
+++ src/Model/Comment.java
@@ -20,7 +20,7 @@ public class Comment {
 
 	private static final String COLLECTION_NAME = "comments";
 	private static final MongoClientURI uri = new MongoClientURI(
-			"mongodb://admin:admin@cluster0-shard-00-00-nvkqp.gcp.mongodb.net:27017,cluster0-shard-00-01-nvkqp.gcp.mongodb.net:27017,cluster0-shard-00-02-nvkqp.gcp.mongodb.net:27017/El-Menus?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
+			"mongodb://localhost");
 
 
 	public static HashMap<String, Object> create(HashMap<String, Object> attributes, String target_id) {
